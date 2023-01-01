e4eaacdb998f83edd353c070b6596cf69c5603fc
Vesela Videva <vesela.videva@gmail.com>
2023-01-02 00:41:30 +0200

    Initial Commit

---
+++ dog-breeds-ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  dog-breeds-ios
+//
+//  Created by Vesela Videva on 2.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dog-breeds-ios/dog_breeds_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dog_breeds_iosApp.swift
+//  dog-breeds-ios
+//
+//  Created by Vesela Videva on 2.01.23.
+//
+
+import SwiftUI
+
+@main
+struct dog_breeds_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
