b90fcee5181018f10582690a403c337b1caf0e58
Vesela Videva <vesela.videva@gmail.com>
2023-01-02 12:41:17 +0200

    First changes - images, basic page

--- dog-breeds-ios/ContentView.swift
+++ dog-breeds-ios/ContentView.swift
@@ -2,20 +2,50 @@
 //  ContentView.swift
 //  dog-breeds-ios
 //
-//  Created by Vesela Videva on 2.01.23.
+//  Created by Vesela Videva on 1.01.23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color
+                .yellow
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("akita")
+                    .resizable()
+                    .cornerRadius(22)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                    .shadow(radius: 16)
+                Text("Japanese Shiba Inu")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.leadinghalf.filled")
+                    Image(systemName: "star")
+                }
+                Spacer()
+                Button("Change card") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.headline)
+                .fontWeight(.bold)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .background(.black)
+                .foregroundColor(.white)
+                .cornerRadius(22)
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
--- dog-breeds-ios/dog_breeds_iosApp.swift
+++ dog-breeds-ios/dog_breeds_iosApp.swift
@@ -2,7 +2,7 @@
 //  dog_breeds_iosApp.swift
 //  dog-breeds-ios
 //
-//  Created by Vesela Videva on 2.01.23.
+//  Created by Vesela Videva on 1.01.23.
 //
 
 import SwiftUI
