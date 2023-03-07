31db1298d3b6ef7ab2c8b9b04fb2ef4565c204c6
Vesela Videva <vesela.videva@gmail.com>
2023-03-07 11:59:30 +0200

    Change dynamically image, breed name and background color

--- dog-breeds-ios/ContentView.swift
+++ dog-breeds-ios/ContentView.swift
@@ -8,49 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var image = "breed1"
+    @State var breed = "Japanese Shiba Inu"
+    @State var zStackColor: Color? = .random
+    
     var body: some View {
         ZStack {
             Color
-                .yellow
+                .random
                 .ignoresSafeArea()
             
             VStack {
-                Image("akita")
+                Image(image)
                     .resizable()
                     .cornerRadius(22)
                     .aspectRatio(contentMode: .fit)
                     .padding(.all)
                     .shadow(radius: 16)
-                Text("Japanese Shiba Inu")
-                    .font(.title)
+                Text(breed)
+                    .font(.largeTitle)
                     .fontWeight(.semibold)
                     .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                HStack {
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.leadinghalf.filled")
-                    Image(systemName: "star")
-                }
+                VStack {
+                    HStack {
+                        Text("Care")
+                        Spacer()
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            Image(systemName: "star")
+                        }
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing]/*@END_MENU_TOKEN@*/)
+                    HStack {
+                        Text("Training")
+                        Spacer()
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            Image(systemName: "star")
+                        }
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing]/*@END_MENU_TOKEN@*/)
+                }.font(.subheadline)
                 Spacer()
-                Button("Change card") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                Button("Change breed") {
+                    changeBreed()
                 }
-                .font(.headline)
-                .fontWeight(.bold)
+                .font(.title2)
+                .fontWeight(.semibold)
                 .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 .background(.black)
                 .foregroundColor(.white)
-                .cornerRadius(22)
+                .cornerRadius(32)
             }
             .padding()
             
         }
+        .background(zStackColor)
+    }
+    
+    func changeBreed() {
+        let num = Int.random(in: 1...3)
+        
+        // Change breed image
+        image = "breed" + String(num)
+            
+        // Change breed name
+        switch num {
+        case 1:
+            breed = "Japanese Shiba Inu"
+        case 2:
+            breed = "German Shepherd"
+        case 3:
+            breed = "Husky"
+        default:
+            break
+        }
+        
+        // Change background color
+        zStackColor = .random
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Color {
+    static var random: Color {
+        return .init(hue: .random(in: 0...1), saturation: 1, brightness: 1)
     }
 }
